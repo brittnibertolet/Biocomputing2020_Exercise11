6bf6cafb01e876293d0472003095d18338d54101
Thomas McCoy <tommymccoy5@gmail.com>
2020-11-10 16:38:35 -0500

    Exercise11work

---
+++ McCoyExercise-11.R
@@ -0,0 +1,44 @@
+# Exercise 11 - Dynamic Modeling
+
+### we started with this basic model
+# time points to simulate over (simulate for 100 years)
+# a place to store population sizes (value of state variable through time)
+# values for our parameters (r=0.2, K=100)
+# a loop for simulating
+# initial starting place; N0=2
+
+# set initial values and parameters
+r=0.1
+K=1000000
+# set a time horizion
+times<-1:50
+# place to store output
+output<-matrix(data=NA,nrow=length(times),ncol=3)
+output[,1]=times
+
+## initial population size
+# for our population of non-resistant tumor cells
+output[1,2]=100
+# for our population of resistant tumors
+output[1,3]=1
+
+# equation w/ for loop
+for(i in times[-1]){
+  output[i,2]=output[(i-1),2]-r*output[(i-1),2]*(1-(output[(i-1),2]+output[(i-1),3])/K)
+  output[i,3]=output[(i-1),3]+r*output[(i-1),3]*(1-(output[(i-1),2]+output[(i-1),3])/K)
+}
+
+# plotting results (first convert from matrix to dataframe)
+outputnonmutant<-data.frame(time=output[,1],nonmutant=output[,2])
+outputmutant<-data.frame(time=output[,1],mutant=output[,3])
+# plot the results
+## load package
+library(ggplot2)
+ggplot()+
+  geom_line(data = outputnonmutant, aes(x=time, y=nonmutant), color='blue')+
+  geom_line(data = outputmutant, aes(x=time, y=mutant), color='red')+
+  xlab("Time")+
+  ylab("# of cells")+
+  theme_classic()
+
+
