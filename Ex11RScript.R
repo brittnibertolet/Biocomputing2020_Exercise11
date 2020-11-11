39d4ff13fdcd0e8d404dd6ea5779a75ed12feac7
Michelle Huang <o.omichelley@gmail.com>
2020-11-11 10:54:17 -0500

    more progress

--- Ex11RScript.R
+++ Ex11RScript.R
@@ -5,7 +5,7 @@ library(ggplot2)
 N0=100
 M0=1
 K=1000000
-timesteps=250
+timesteps=500
 rN=0.1
 rM=0.05
 
@@ -37,20 +37,20 @@ for(t in 1:(timesteps-1)){
 }
 
 
-# plot simulation
-sim1<-data.frame(time=1:length(N))
+# create data frame
+sim1<-data.frame(name=sim1, time=1:length(N), N=N)
+sim2<-data.frame(name=sim2, time=1:length(M), M=M)
+
+# plot simulations
 ggplot(data=sim1)+
-  geom_line(aes(x=time,y=N),col='black')+
-  geom_line(aes(x=time,y=N),col='red')+
+  geom_line(aes(x=time,y=N),col='blue')+
   theme_classic()
-sim2<-data.frame(time=1:length(M))
 ggplot(data=sim2)+
-  geom_line(aes(x=time,y=M),col='black')+
   geom_line(aes(x=time,y=M),col='red')+
   theme_classic()
 
 # combine simulations
-dat <- rbind(sim1, sim2)
+dat <- rbind(sim1,sim2)
 
 # plot
 ggplot(data=dat,aes(x=time))+geom_line(aes(color=name))+theme_classic()
