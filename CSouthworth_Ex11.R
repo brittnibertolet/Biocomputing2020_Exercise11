61deba8a2e374eb07d3c65ba8dac386aa1449a1c
chelsea307 <71391201+chelsea307@users.noreply.github.com>
2020-11-11 14:35:39 -0500

    Add files via upload

---
+++ CSouthworth_Ex11.R
@@ -0,0 +1,27 @@
+# Exercise 11
+# Chelsea Southworth
+
+# load ggplot for later figures
+library(ggplot2)
+
+### we started with this basic model
+# time points to simulate over (simulate for 100 years)
+# a place to store population sizes (value of state variable through time)
+# values for our parameters (r=0.2, K=100)
+# a loop for simulating
+# initial starting place; N0=2
+
+
+
+
+# set parameter and state variable values
+rn = .1
+rm = .1
+K = 1000000
+timesteps = 1 # <- this isn't supposed to be one, change it
+# not sure how many timesteps we need to run the model to equilibrium?
+
+# create vector to store N's and set initial N
+
+
+# create vector to store M's and set initial M
