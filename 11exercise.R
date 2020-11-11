af3cb0507f3cdc11078917c32b9e9fbae92aba76
Laura Carroll <lcarroll916@gmail.com>
2020-11-11 15:59:19 -0500

    Added in drug treatment affects

--- 11exercise.R
+++ 11exercise.R
@@ -30,16 +30,24 @@ M[1] <- 1
 
 ##Step 5: Set up the for loop to fill in the cancerCells matrix
 for (i in 2:length(1:timepts)){
+  if (i>=250){
+    #These include the rates at which the cells grow in the presence of medicine
+    #I chose 250, because by this point the graph had reached equilibrium
+    N[i] = N[i-1] + rNm*N[i-1]*(1-(N[i-1]+M[i-1])/K)
+    M[i] = M[i-1] + rMm*M[i-1]*(1-(M[i-1]+N[i-1])/K)
+  }else{
     N[i] = N[i-1] + rN*N[i-1]*(1-(N[i-1]+M[i-1])/K)
     M[i] = M[i-1] + rM*M[i-1]*(1-(M[i-1]+N[i-1])/K)
+  }
 }
 
 ##Step 6: Create a data frame with this info so that it may be plotted
 cancerCells <- data.frame(timesteps=1:timepts, Nval=N, Mval=M)
 
 ##Step 7: Plot the data in a line graph
+#The cells reach equilibrium before 250 timesteps
 ggplot(data=cancerCells, aes(x=timesteps))+
   geom_line(aes(y=Nval, color = "Non-Mutant Cells"))+
   geom_line(aes(y=Mval, color = "Mutant Cells"))+
-  labs(color="Cell Type")+
+  labs(color="Cell Type", x="Time (Days)", y="Cell Count")+
   theme_classic()
