621e0f93a252d337657cac0f8f5903b6b8008e0d
jennalehn <jenna.lehn@gmail.com>
2020-11-11 20:38:21 -0500

    pretty fixes

--- Ex11.R
+++ Ex11.R
@@ -8,7 +8,7 @@
 r=0.1 #normal growth rate of cells both rM and rN
 K=1000000 #carrying capacity
 N0=2 #original population number
-time=500 
+time=600 
 
 #output for loop
 N=numeric(length=length(1:time))
@@ -30,7 +30,7 @@ head(find100[find100$N>99,])
 #time=42 when reaches 100 and mutation occurs
 M[42]=1
 
-#for loop
+#for loop to make values for graph
 for (i in 2:length(1:time)){
   # at N=100, mutant cells start to grow
   if (i>42 & i<250){
@@ -38,11 +38,12 @@ for (i in 2:length(1:time)){
     N[i]=N[i-1]+r*N[i-1]*(1-(N[i-1]+M[i-1])/K)
     # after reach equilibrium, introduce drug
   }else if (i>250){
+    M[250]=9944.072
     # growth rate of mutants is half
     M[i]=(M[i-1]+0.5*r*M[i-1]*(1-(N[i-1]+M[i-1])/K)) 
     # growth rate of non-mutants is -r
     N[i]=N[i-1]+(-r)*N[i-1]*(1-(N[i-1]+M[i-1])/K) 
-    # in the beginning, only non-mutant cells grow 
+  # in the beginning, only non-mutant cells grow 
   }else{
     N[i]=N[i-1]+r*N[i-1]*(1-(N[i-1]+M[i-1])/K)
   }
@@ -54,17 +55,14 @@ library(ggplot2)
 #make data frame for graph
 simN=data.frame(time=1:time,N=N,Cells="Non-Mutant")
 simM=data.frame(time=1:time,N=M,Cells="Mutant")
-simN
-simM
 
 #combine to one data frame
 dat=rbind(simN,simM)
-dat
 
 #make line graph
 ggplot(data=dat,aes(x=time,y=N))+
   geom_line(aes(color=Cells))+
   geom_vline(xintercept=250,linetype="dotted")+
-  annotate("text",x=350,y=1000000,label="Drug Added",size=3)+
+  annotate("text",x=400,y=1000000,label="Drug Added",size=3)+
   theme_classic()
 
