123eb8f0598f90228bd45bd52d8fa25e157873ee
ilickona0 <ilickona@gmail.com>
2020-11-12 15:35:01 -0500

    trying to make place to store data

--- IsaacL_GrowthModel.R
+++ IsaacL_GrowthModel.R
@@ -1,11 +1,15 @@
 #Exercise 11 - modeling growth of populations
 
 #Parameters
+K <- 1000000 #carrying capacity
 r<- 0.1 #normal growth
-rND<- 0.1
+rnormND<- -0.1 #normal cells decline
 rD<- 0.05 #drug growth
 
-K <- 1000000 #carrying capacity
+time <- 1:1000 #time values for graphing
 
-time <- 1:1000
+#Initialize dataframe for outputs
+
+resevoir <- data.frame(data=NA, nrow=1000, ncol = 3)
+resevoir[,1] <- time
 
