0da9310f082e1058aeae3121712178a052925048
ilickona0 <ilickona@gmail.com>
2020-11-12 17:04:18 -0500

    issues pushing to github

--- IsaacL_GrowthModel.R
+++ IsaacL_GrowthModel.R
@@ -10,9 +10,9 @@ time <- 1:1000 #time values for graphing
 
 #Initialize sites for outputs
 
-resevoirN <- numeric(length=length(1:time))
+resevoirN <- numeric(length=length(time))
 resevoirN[1]<- 99 #initial number of normal cells after mutation occurs
-resevoirM <- numeric(length = length(1:time))
+resevoirM <- numeric(length = length(time))
 resevoirM[1] <- 1 #first mutated cell
 
 #For loop to calculate graph values with equations
@@ -21,11 +21,11 @@ resevoirM[1] <- 1 #first mutated cell
                   # Mt+1 =Mt+rMMt(1−(Nt+Mt)/K)
 
 for(i in 2:length(time)){
-  #start with normal growth
-  if(i<=100){
+  #start with normal growth (300 is around equilibrium)
+  if(i<=300){ 
   resevoirM[i] <- resevoirM[i-1] + rN*resevoirM[i-1]*(1-(resevoirM[i-1]+resevoirN[i-1])/K)
   resevoirN[i] <- resevoirN[i-1] + rN*resevoirN[i-1]*(1-(resevoirM[i-1]+resevoirN[i-1])/K)
-  }else if(i>100){
+  }else if(i>300){
     resevoirM[100]<-5.755493e+03
 
     resevoirM[i]<- resevoirM[i-1] + rMd*resevoirM[i-1]*(1-(resevoirM[i-1]+resevoirN[i-1])/K)
@@ -37,8 +37,13 @@ for(i in 2:length(time)){
 frame <- data.frame(Time = time, N=resevoirN,M=resevoirM)
 library(ggplot2)
 
-ggplot(data=frame, aes(x=time, y=N))+
-  geom_line(aes(y=M))+
+ggplot(data=frame, aes(x=time))+
+  geom_line(aes(y=M, color = "Cancer Cells"))+
+  geom_line(aes(y=N, color = "Normal Cells"))+
+  geom_vline(xintercept=300,linetype="dotted")+
+  annotate("text", x=300, y=500000, label="Drug Introduced", size = 2)+
+  ylab("Number of Cells")+
+  xlab("Time")
   theme_classic()
 
 
