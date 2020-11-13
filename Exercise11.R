89bdccf5da431140c0bd10a9b4939b9b936726c6
mpjlicursi <matthewlicursi@Matthews-MacBook-Pro.local>
2020-11-12 20:52:16 -0500

    Commented and Final Code

--- Exercise11.R
+++ Exercise11.R
@@ -2,11 +2,13 @@
 
 #Set up initial parameters
 #When drug is absent:
-rN<- 0.1
-rM<- 0.1
+rN<- 0.1 #Rate of normal cell growth
+rM<- 0.1 #Rate of mutant cell growth
+
 #When drug is present
-rN2<- -0.1
-rM2<- 0.05
+rN2<- -0.1 #Rate of normal cell growth
+rM2<- 0.05 #Rate of mutant cell growth
+
 #Carrying Capacity
 K<- 1000000
 
@@ -18,14 +20,14 @@ N<-numeric(length=length(timesteps))
 M<-numeric(length=length(timesteps))
 
 #Set N0 and M0
-N0<-99
-M0<-1
+N0<-99 #Initial normal cell population
+M0<-1 #Initial mutant cell population
 N[1]<-N0
 M[1]<-M0
 
 #Use a for loop to simulate in time
 for(i in 1:timesteps){
-  if(i<200){
+  if(i<200){ 
     M[i+1] = M[i]+rM*M[i]*(1-(N[i]+M[i])/K)
     N[i+1] = N[i]+rN*N[i]*(1-(N[i]+M[i])/K)
   }else{
@@ -34,16 +36,17 @@ for(i in 1:timesteps){
   }
 }
 
-#Set up a drug treatment event to occur sometime at t around 200
-TotalCells<-data.frame(time=1:(timesteps+1),N=N,M=M)
+#Create dataframe for data
+TotalCells<-data.frame(time=1:(timesteps+1),N,M)
 
+#Plot data using ggplot
 library(ggplot2)
-ggplot(TotalCells, aes(x=time, color = N))+
-  geom_line(aes(y=N), color="red") +
-  geom_line(aes(y=M), color="blue") +
-  xlab("Time") +
-  ylab("Number of Cells") +
-  geom_vline(xintercept = c(200), linetype = "dotted") +
+ggplot(TotalCells, aes(x=time))+
+  geom_line(aes(y=N), color="red") + #plot the normal strain data
+  geom_line(aes(y=M), color="blue") + #plot the mutant strain data
+  xlab("Time") + #Label thr x-axis
+  ylab("Number of Cells") + #Label the y-axis
+  geom_vline(xintercept = c(200), linetype = "dotted") + #Set up a drug treatment event to occur sometime at t around 200
   theme_classic()
 
 
